6352140b4ad08e9693db16ab791aa7d0b7003fe2
Galvin <b02437@gmail.com>
2019-02-13 00:13:14 +0800

    init project

---
+++ TinyCreditCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinyCreditCard
+//
+//  Created by Galvin on 2019/2/12.
+//  Copyright © 2019 @GalvinLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
